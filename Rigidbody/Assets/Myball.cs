8fce9dd5616c831c4a1ba59abec9b6ab7768de2e
guemin96 <guemin12@naver.copm>
2021-07-11 15:38:58 +0900

    20210711

--- Rigidbody/Assets/Myball.cs
+++ Rigidbody/Assets/Myball.cs
@@ -29,11 +29,23 @@ public class Myball : MonoBehaviour
         Vector3 vec = new Vector3(Input.GetAxis("Horizontal"),
                                     0,
                                     Input.GetAxis("Vertical"));
+        
         rigid.AddForce(vec, ForceMode.Impulse);//
         rigid.AddTorque(Vector3.back);
 
 
         //rigid.velocity = new Vector3(2, 4, -1);//현재 이동속도
 
+    }
+    void OnTriggerStay(Collider other)//투명한 상자로 공이 이동했을때 공이 위로 붕뜨게 해주는 함수
+    {
+        if (other.name == "Cube")
+        {
+            rigid.AddForce(Vector3.up, ForceMode.Impulse);
+        }
+    }
+    public void Jump()
+    {
+        rigid.AddForce(Vector3.up*20, ForceMode.Impulse);
     }
 }
